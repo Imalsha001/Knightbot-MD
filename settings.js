b3bc61d9bb5c4d332c10d8f652d4005e8e5265a6
Imalsha001 <imalsharandipabc@gmail.com>
2025-05-11 12:22:38 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,12 +1,12 @@
 const settings = {
-  packname: 'Knight Bot',
-  author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  packname: '★彡[ᴋɴɪɢʜᴛ ʙᴏᴛ ᴠ2]彡★',
+  author: '‎ミ★ 𝘐𝘔𝘈𝘓𝘚𝘏𝘈 ★彡',
+  botName: "ミ★ 𝘒𝘕𝘐𝘎𝘏𝘛 𝘉𝘖𝘛 ★彡",
+  botOwner: '•´¯`•.IMALSHA.•´¯`•', // Your name
+  ownerNumber: '94711683227', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
-  description: "This is a bot for managing group commands and automating tasks.",
+  description: "This is a bot for managing group commands and automating tasks. this bot maked with java script and python languages by IMALSHA RANDIP",
   version: "2.0.0",
 };
 
